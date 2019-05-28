c1d2d4b26bf628015a6cbcfd8eb444c7eb4041f7
isobe <ngnp.pngn@icloud.com>
2019-05-28 19:30:22 +0900

    GoForwardUnityChanプロジェクト
    Lesson7課題　キューブ接触時の効果音

---
+++ Assets/CubeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	private float speed = -0.2f;
+	private float deadLine = -10;
+	public AudioClip block;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (this.speed, 0, 0);
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "UnityChan") {
+			GetComponent<AudioSource> ().volume = 0;
+		} 
+
+		if (other.gameObject.tag == "Ground" || tag == "Cube") {
+			audioSource.PlayOneShot (block);
+		}
+	}
+}
